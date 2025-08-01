194ae7797cf7a18eb2358e0bfdf631c86458a131
AlejoGDOfficial <alejogdoff@gmail.com>
2025-08-01 16:41:12 -0500

    epic rulescript fixes

--- setup/install-haxelibs.bat
+++ setup/install-haxelibs.bat
@@ -10,7 +10,7 @@ haxelib install lime 8.2.2
 haxelib install openfl 9.4.1
 haxelib install flixel-addons 3.3.2
 haxelib git hxcpp https://github.com/AlejoGDOfficial/MobilePorting-hxcpp --skip-dependencies
-haxelib git rulescript https://github.com/Kriptel/RuleScript 7bea5b4e8b137f112523f1fa41b6ac7e11f51df0 --skip-dependencies
+haxelib git rulescript https://github.com/Kriptel/RuleScript 67c0073317318c7260d47a0d13071a60decfd50d --skip-dependencies
 haxelib git hscript https://github.com/HaxeFoundation/hscript 04e7d656b667f375bbe58ee10082aee2850a3f9c
 haxelib install tjson 1.4.0
 haxelib install extension-androidtools 2.2.1 --skip-dependencies
